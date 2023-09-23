9483f1905b83275bb60d4a2c168ca5b36ad3c368
monarxxjain <jainmonark1000@gmail.com>
2023-09-23 12:23:13 +0530

    mongoURL from env

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017';
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
